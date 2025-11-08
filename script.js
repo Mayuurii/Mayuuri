d65755ccddc41a138730d376feb6af2aa7f3c8b8
mayuurii <wfwafawfawfawfawf@gmail.com>
2025-11-08 22:48:40 +0100

    ajout du site MyBio

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-// Petit effet d'apparition smooth
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector(".card").style.opacity = "0";
-  setTimeout(() => {
-    document.querySelector(".card").style.transition = "opacity 1s";
-    document.querySelector(".card").style.opacity = "1";
-  }, 300);
-});
+// Tu pourras plus tard ajouter des effets ici (par ex. particules, texte animé, etc.)
