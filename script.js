98c983971440f032a3626fbce41c98015ce17294
mayuurii <wfwafawfawfawfawf@gmail.com>
2025-11-11 18:22:13 +0100

    ajout du site MyBio

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+/* === INTRO CLICK === */
+const intro = document.getElementById("intro");
+
+// Fade + remove
+intro.addEventListener("click", () => {
+  intro.classList.add("fade-out");
+
+  setTimeout(() => {
+    intro.style.display = "none";
+  }, 600);
+});
+
+
+/* === CARD 3D === */
 const card = document.getElementById('card');
 const MAX_ROT = 20;
 const SCALE = 1.05;
@@ -7,15 +21,12 @@ card.addEventListener('mousemove', (e) => {
   const centerX = rect.left + rect.width / 2;
   const centerY = rect.top + rect.height / 2;
 
-  // Position relative au centre
   const x = e.clientX - centerX;
   const y = e.clientY - centerY;
 
-  // Normalisation entre -1 et 1
   const percentX = x / (rect.width / 2);
   const percentY = y / (rect.height / 2);
 
-  // Application de la rotation inversée pour le réalisme
   const rotateY = percentX * MAX_ROT;
   const rotateX = -percentY * MAX_ROT;
 
@@ -37,12 +48,3 @@ card.addEventListener('mouseleave', () => {
   `;
   card.style.transition = 'transform 0.3s ease';
 });
-``
-
-// INTRO CLICK
-const intro = document.getElementById("intro");
-
-intro.addEventListener("click", () => {
-  intro.classList.add("fade-out");
-});
-
